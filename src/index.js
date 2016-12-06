9a6089203715c7155437f74ad49cd10e484df391
Nicolas Bazille <oltodo@msn.com>
2016-12-06 10:06:56 +0100

    Install Draftjs (#3)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import Editor from './Editor';
+
+const Drinkjs = {};
+
+Drinkjs.Editor = function (element) {
+  ReactDOM.render(<Editor />, element);
+};
+
+window.Drinkjs = Drinkjs;
