f0448babd7146a223b129725eb84773fe1b07337
y-poli <ypoli.externe@m6.fr>
2016-12-07 11:19:14 +0100

    Add blocks type button to toolbar

---
+++ src/icons/HeadingThreeIcon.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default
+class HeadingThreeIcon extends Component {
+  render() {
+    return (
+      <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7 10v3h3.5v6h3V13H17V10z" />
+        <path d="M0 0h24v24H0V0z" fill="none" />
+      </svg>
+    );
+  }
+}
