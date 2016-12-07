7c65eb538d31f384bf95e7e0370f604ae121371f
y-poli <ypoli.externe@m6.fr>
2016-12-07 11:19:14 +0100

    Add blocks type button to toolbar

--- src/Editor.js
+++ src/Editor.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-
 import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
 
 import createInlineToolbarPlugin, {
   createInlineStyleButton,
+  createBlockStyleButton,
+  Separator,
 } from './plugins/toolbar';
 
 import BoldIcon from './icons/BoldIcon';
 import ItalicIcon from './icons/ItalicIcon';
 import UnderlineIcon from './icons/UnderlineIcon';
 import StrikethroughIcon from './icons/StrikethroughIcon';
+import HeadingOneIcon from './icons/HeadingOneIcon'
+import HeadingTwoIcon from './icons/HeadingTwoIcon'
+import HeadingThreeIcon from './icons/HeadingThreeIcon'
+import BlockquoteIcon from './icons/BlockquoteIcon'
+
+import 'draft-js/dist/Draft.css';
+import './Editor.css'
 
 const inlineToolbarPlugin = createInlineToolbarPlugin({
   buttons: [
@@ -17,10 +25,13 @@ const inlineToolbarPlugin = createInlineToolbarPlugin({
     createInlineStyleButton({ style: 'ITALIC', children: <ItalicIcon /> }),
     createInlineStyleButton({ style: 'UNDERLINE', children: <UnderlineIcon /> }),
     createInlineStyleButton({ style: 'STRIKETHROUGH', children: <StrikethroughIcon /> }),
+    Separator,
+    createBlockStyleButton({ blockType: 'header-one', children: <HeadingOneIcon /> }),
+    createBlockStyleButton({ blockType: 'header-two', children: <HeadingTwoIcon /> }),
+    createBlockStyleButton({ blockType: 'header-three', children: <HeadingThreeIcon /> }),
+    createBlockStyleButton({ blockType: 'blockquote', children: <BlockquoteIcon /> }),
   ]
 });
-
-import 'draft-js/dist/Draft.css';
 
 const defaultText = 'Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Praesent egestas tristique nibh. Aenean massa. Pellentesque dapibus hendrerit tortor. Nam commodo suscipit quam. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Nullam dictum felis eu pede mollis pretium. In ac felis quis tortor malesuada pretium. Aenean ut eros et nisl sagittis vestibulum. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros.';
 
