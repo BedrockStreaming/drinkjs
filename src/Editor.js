0ed8f49a9da5e84e12b6fa3837fa05bbcbd09c3f
Nicolas Bazille <oltodo@msn.com>
2016-12-08 07:44:14 +0100

    Divide Editor.js in two parts and add a default state

--- src/Editor.js
+++ src/Editor.js
@@ -1,55 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
-
-import createInlineToolbarPlugin, {
-  createInlineStyleButton,
-} from './plugins/toolbar';
-
-import BoldIcon from './icons/BoldIcon';
-import ItalicIcon from './icons/ItalicIcon';
-import UnderlineIcon from './icons/UnderlineIcon';
-import StrikethroughIcon from './icons/StrikethroughIcon';
-
-const inlineToolbarPlugin = createInlineToolbarPlugin({
-  buttons: [
-    createInlineStyleButton({ style: 'BOLD', children: <BoldIcon /> }),
-    createInlineStyleButton({ style: 'ITALIC', children: <ItalicIcon /> }),
-    createInlineStyleButton({ style: 'UNDERLINE', children: <UnderlineIcon /> }),
-    createInlineStyleButton({ style: 'STRIKETHROUGH', children: <StrikethroughIcon /> }),
-  ]
-});
+import Draft, { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import Immutable from 'immutable';
 
 import 'draft-js/dist/Draft.css';
 
-const defaultText = 'Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Praesent egestas tristique nibh. Aenean massa. Pellentesque dapibus hendrerit tortor. Nam commodo suscipit quam. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Nullam dictum felis eu pede mollis pretium. In ac felis quis tortor malesuada pretium. Aenean ut eros et nisl sagittis vestibulum. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros.';
+import styles from './Editor.css';
+
+const blockRenderMap = Immutable.Map({
+  'paragraph': {
+    element: 'paragraph'
+  },
+  'unstyled': {
+    element: 'paragraph'
+  }
+ });
+
+const extendedBlockRenderMap = Draft.DefaultDraftBlockRenderMap.merge(blockRenderMap);
 
 class DrinkEditor extends Component {
+  static propTypes = {
+    state: PropTypes.object,
+    readOnly: PropTypes.bool,
+    onChange: PropTypes.func,
+    plugins: PropTypes.array,
+  }
+
   constructor(props) {
     super(props);
 
+    const {
+      state,
+      onChange = () => {},
+    } = props;
+
     this.state = {
-      editorState: createEditorStateWithText(defaultText),
+      editorState: state ?
+        EditorState.push(EditorState.createEmpty(), convertFromRaw(state)) :
+        EditorState.createEmpty(),
     };
 
-    this.onChange = (editorState) => this.setState({editorState});
+    this.onChange = (editorState) => {
+      onChange(convertToRaw(editorState.getCurrentContent()));
+      this.setState({ editorState });
+    };
   }
 
   render() {
     const { editorState } = this.state;
-    const { InlineToolbar } = inlineToolbarPlugin;
+
+    const {
+      plugins = [],
+      readOnly = false,
+    } = this.props;
 
     return (
-      <div>
+      <div className={styles.editor}>
         <Editor
           editorState={editorState}
           onChange={this.onChange}
           placeholder="Write something..."
-          plugins={[
-            inlineToolbarPlugin,
-          ]}
+          blockRenderMap={extendedBlockRenderMap}
+          readOnly={readOnly}
+          plugins={plugins}
         />
-        <InlineToolbar />
+
+        {plugins.reduce((prev, curr) => (
+          curr.InlineToolbar ? React.createElement(curr.InlineToolbar) : prev
+        ), null)}
       </div>
     );
   }
