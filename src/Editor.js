927bf33a57bd4233ef27c037b8d5b0b22211bc4b
Ludovic Lasry <llasry@ludo-mac.local>
2017-01-06 11:47:45 +0100

    wipe

--- src/Editor.js
+++ src/Editor.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
-import Draft, { EditorState, RichUtils, convertFromRaw, convertToRaw } from 'draft-js';
+import Draft, { ContentState,EditorState, RichUtils, convertFromRaw, convertToRaw } from 'draft-js';
 import Editor from 'draft-js-plugins-editor';
 import Immutable from 'immutable';
 
@@ -54,6 +54,8 @@ class DrinkEditor extends Component {
       const maxDepth = 4;
       this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
     };
+
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
   }
 
   renderPlugins() {
@@ -72,7 +74,128 @@ class DrinkEditor extends Component {
     }, []);
   }
 
+  handleKeyCommand(command: string): DraftHandleValue {
+    console.log(command);
+    const { editorState } = this.state;
+    if (command === 'deletee') {
+
+      var contentState = editorState.getCurrentContent();
+      var selectionState = contentState.getSelectionAfter();
+      var blockMap = contentState.getBlockMap();
+      var startKey = selectionState.getStartKey();
+      var endKey =  editorState.getCurrentContent().getKeyAfter(startKey);
+      var startOffset = selectionState.getStartOffset();
+      var endOffset = selectionState.getEndOffset();
+      var startBlock = blockMap.get(startKey);
+      var endBlock = blockMap.get(endKey);
+      console.log(startBlock);
+      console.log(endBlock);
+      console.log("startOffset " + startOffset);
+      console.log("endOffset " + endOffset);
+      console.log("startKey " + startKey);
+      console.log("endKey " + endKey);
+      var newstate;
+      console.log(startBlock.getText());
+      if(startBlock.getText().length == 0)
+      {console.log("in length");
+        var modifiedStart = startBlock.merge({
+          text: startBlock.getText().slice(0, startOffset) + endBlock.getText().slice(endOffset),
+          type: endBlock.getType()
+        });
+
+        console.log(modifiedStart);
+        console.log(blockMap);
+        console.log("blockMap");
+        
+        const skipuntilvalue = blockMap.toSeq().skipUntil(function (_, k) {
+          return k === startKey;
+        });
+        console.log("skipuntil");
+        console.log(skipuntilvalue);
+        
+        const takeuntilvalue = skipuntilvalue.takeUntil(function (_, k) {
+          return k === endKey;
+        }) ;
+        console.log("takeuntil");
+        console.log(takeuntilvalue);
+
+        var newBlocks = blockMap.toSeq().skipUntil(function (_, k) {
+          return k === startKey;
+        }).takeUntil(function (_, k) {
+          return k === endKey;
+        }).concat(Immutable.Map([[endKey, null]])).map(function (_, k) {
+          return k === startKey ? modifiedStart : null;
+        });
+
+
+
+         blockMap = blockMap.merge(newBlocks).filter(function (block) {
+          return !!block;
+        });
+
+        console.log(blockMap);
+        console.log("newBlocks");
+
+          const newContentState = contentState.merge({
+            blockMap: blockMap,
+            selectionBefore: selectionState,
+            selectionAfter: selectionState.merge({
+              anchorKey: startKey,
+              anchorOffset: startOffset,
+              focusKey: startKey,
+              focusOffset: startOffset,
+              isBackward: true
+            })
+          });
+
+
+          console.log("contentState");
+          console.log(contentState);
+          console.log("newContentState");
+          console.log(newContentState);
+         
+          var newContentState2 = ContentState.createFromText("text");
+
+          newContentState2 = newContentState2.merge({
+            blockMap: blockMap
+          });
+          console.log("newContentState2");
+          console.log(newContentState2);
+          
+          console.log("editorState");
+          console.log(editorState);
+
+
+
+
+          const newEditorState = EditorState.push(editorState, newContentState);
+
+          
+          // get all active inline style
+          const currentInlineStyles = editorState.getCurrentInlineStyle().toArray();
+
+          console.log("newEditorState");
+          console.log(newEditorState);
+
+          // toggle style
+          const nextEditorState = currentInlineStyles.reduce((editorState, style) => (
+            RichUtils.toggleInlineStyle(editorState, style)
+          ), newEditorState);
+
+        if (newEditorState) {
+            this.onChange(newEditorState);
+            return 'handled';
+          }
+
+        return 'handled';
+      }else
+        return 'not-handled'
+    }
+    return 'not-handled';
+  }
+
   render() {
+    console.log("render");
     const { editorState } = this.state;
     const { plugins, readOnly } = this.props;
 
@@ -80,6 +203,7 @@ class DrinkEditor extends Component {
       <div className={styles.editor}>
         <Editor
           editorState={editorState}
+          handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange}
           onTab={this.onTab}
           placeholder="Write something..."
