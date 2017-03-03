3a67eeea451754656123f21b3ec0b7b397327a6c
TomRibuot <tom.ribuot@gmail.com>
2017-03-03 10:51:11 +0100

    Fix style removed (#45)
    * fix delete block issue that remove last block style
    
    fix handlecommand on editor
    
    remove comments
    
    change variable name
    
    * fixed comments & code style

--- src/Editor.js
+++ src/Editor.js
@@ -74,6 +74,92 @@ class DrinkEditor extends Component {
       onChange(convertToRaw(editorState.getCurrentContent()));
       this.setState({ editorState });
     };
+
+    this.onTab = (e) => {
+      const maxDepth = 4;
+      this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
+    };
+
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  renderPlugins() {
+    const { plugins } = this.props;
+
+    return plugins.reduce((prev, curr) => {
+      curr.InlineToolbar && prev.push(React.createElement(curr.InlineToolbar, {
+        key: 'inline-toolbar',
+      }));
+
+      curr.SideToolbar && prev.push(React.createElement(curr.SideToolbar, {
+        key: 'side-toolbar',
+      }));
+
+      return prev;
+    }, []);
+  }
+
+  handleKeyCommand(command: string): DraftHandleValue {
+
+    const { editorState } = this.state;
+
+    if (command === 'delete' ) {
+
+      const contentState = editorState.getCurrentContent();
+      const selectionState = editorState.getSelection();
+      const blockMap = contentState.getBlockMap();
+      const startKey = selectionState.getStartKey();
+      const endKey = selectionState.getEndKey();
+      const startOffset = selectionState.getStartOffset();
+      const endOffset = selectionState.getEndOffset();
+      const startBlock = blockMap.get(startKey);
+      const endBlock = blockMap.get(endKey);
+      let mergedBlocks = [];
+
+      // change the way to delete blocks if startblock is empty or if last block is not fully deleted
+      if (startOffset === 0) {
+
+        const blocksBefore = blockMap.toSeq().takeUntil((_, v) => (v === startKey));
+        const blocksAfter = blockMap.toSeq().skipUntil((_, v) => (v === endKey)).rest();
+        
+        // get focus key (would be the block just after the last deleted one)
+        let focusKey = editorState.getCurrentContent().getKeyAfter(endKey);
+
+        // if part of lastblock must be kept, create a new block with remaining text
+        if (endOffset < endBlock.getLength()) {
+          let modifiedEndBlock = endBlock.merge({
+            text: endBlock.getText().slice(endOffset),
+            type: endBlock.getType()
+          });
+
+          mergedBlocks.push([modifiedEndBlock.getKey(), modifiedEndBlock]);
+          focusKey = modifiedEndBlock.getKey();
+        }
+
+        const newBlocks = blocksBefore.concat(mergedBlocks, blocksAfter).toOrderedMap();
+
+        const newContentState = contentState.merge({
+          blockMap: newBlocks,
+          selectionBefore: selectionState,
+          selectionAfter: selectionState.merge({
+            anchorKey: focusKey,
+            anchorOffset: 0,
+            focusKey: focusKey,
+            focusOffset: 0,
+            isBackward: false
+          })
+        });
+
+        const newEditorState = EditorState.push(editorState, newContentState);
+
+        if (newEditorState) {
+          this.onChange(newEditorState);
+          return 'handled';
+        }
+      }
+    }
+
+    return 'not-handled';
   }
 
   renderPlugins() {
@@ -100,6 +186,7 @@ class DrinkEditor extends Component {
           placeholder="Write something..."
           blockRenderMap={extendedBlockRenderMap}
           readOnly={readOnly}
+          handleKeyCommand={this.handleKeyCommand}
           plugins={[basePlugin, ...plugins]}
         />
 
